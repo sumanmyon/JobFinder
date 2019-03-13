c86f31892955c92258f31610b550efbaec44356d
Suman Neupne <sumanmyon.sam@gmail.com>
2019-03-13 08:28:39 +0545

    Getting data from provider to show in MainActivity

--- app/src/main/java/www/sumanmyon/com/jobfinder/MainActivity.java
+++ app/src/main/java/www/sumanmyon/com/jobfinder/MainActivity.java
@@ -2,14 +2,27 @@ package www.sumanmyon.com.jobfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import www.sumanmyon.com.jobfinder.FetchData.GetDataFromProvider;
+
+import static www.sumanmyon.com.jobfinder.URLs.ProviderURLs.GitHubURL;
+import static www.sumanmyon.com.jobfinder.URLs.ProviderURLs.searchGovURL;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
 
+        listView = (ListView)findViewById(R.id.main_activity_listView);
+
+        //get data from provider
+        GetDataFromProvider getDataFromProvider = new GetDataFromProvider(this,listView);
+        getDataFromProvider.getData(GitHubURL,"GitHub");
+        getDataFromProvider.getData(searchGovURL,"searchGov");
     }
 }
