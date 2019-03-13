345f1d1e67af7032f35390b084f68825f055c002
Suman Neupne <sumanmyon.sam@gmail.com>
2019-03-13 11:18:12 +0545

    Fixing errors

--- app/src/main/java/www/sumanmyon/com/jobfinder/URLs/GitHubURL.java
+++ app/src/main/java/www/sumanmyon/com/jobfinder/URLs/GitHubURL.java
@@ -1,6 +1,6 @@
 package www.sumanmyon.com.jobfinder.URLs;
 
-public class ProviderURLs {
+public class GitHubURL {
     public static final String GitHubURL = "https://jobs.github.com/positions.json";
     public static final String searchGovURL ="https://jobs.search.gov/jobs/search.json";
 }
