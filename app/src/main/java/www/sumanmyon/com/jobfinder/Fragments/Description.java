a876ae723c4cded3757e57dafcf240ebde53f4ac
Suman Neupne <sumanmyon.sam@gmail.com>
2019-03-13 11:28:32 +0545

    Creting fragment to show description of company

--- app/src/main/java/www/sumanmyon/com/jobfinder/Fragments/Description.java
+++ app/src/main/java/www/sumanmyon/com/jobfinder/Fragments/Description.java
@@ -24,6 +24,7 @@ public class Description extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_description, container, false);
     }
 
